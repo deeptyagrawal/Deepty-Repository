ccecc860fa7eb8f81ca04db42c431b46fe2bd7ef
deeptyagrawal <56798832+deeptyagrawal@users.noreply.github.com>
2019-10-21 09:45:19 -0500

    Pushing Java Program

---
+++ Assignment2.java
@@ -0,0 +1,41 @@
+package assignment2;
+
+import java.util.Scanner;
+
+public class Assignment2 {
+
+		public static void main(String[] args) {
+			
+			//1.Ask the USER for a string (it should print "Enter your Name”)
+			//a.assume the user enters "His/her name"
+				System.out.println("Enter Your Name");
+				Scanner sc = new Scanner(System.in);
+				String name = sc.next();
+				System.out.println(name);
+				
+			//2.Convert that string to all uppercase and print it (it should print the input in uppercase)
+				System.out.println(name.toUpperCase());
+				
+			//3.Print the length of that string (it should print as per the input given by user)
+				System.out.println(name.length());
+				
+			//4.Replace 'y’ with ‘i’ (it should print as per the user input)
+				String newname = name.replace("y", "i");
+				System.out.println(newname);
+				
+			//5.Give the last char of the string (it should print last character of the input)
+				int allchar = newname.length();
+				String lastchar = newname.substring(allchar-2);
+				System.out.println(lastchar);
+				
+			//6.Concatenate the string from step 1 with string "Test" (it should print User name +"Tester”)
+				System.out.println(name + " Tester");
+				
+				System.out.println(name.charAt(5));
+				
+				
+			
+
+	}
+
+}
